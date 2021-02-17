601965bf56268a86add29d8fd53f010dc234635e
Andrej Flieger <AFG@host.docker.internal>
2021-02-17 13:36:12 +0100

    Change to XML Configuration. Uaaaaah. :)

--- src/main/java/guru/springframework/joke/config/ChuckConfiguration.java
+++ src/main/java/guru/springframework/joke/config/ChuckConfiguration.java
@@ -1,18 +1,18 @@
 package guru.springframework.joke.config;
 
-import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
 /**
  * Created by jt on 5/25/17.
  */
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
-    @Bean
-    public ChuckNorrisQuotes chuckNorrisQuotes(){
-        return new ChuckNorrisQuotes();
-    }
+	@Bean
+	public ChuckNorrisQuotes chuckNorrisQuotes() {
+		return new ChuckNorrisQuotes();
+	}
 
 }
